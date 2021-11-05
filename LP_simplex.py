e0991c5e7b864e87759ff74f0e16d82c0ced32a9
phamson02 <phamtienson02@gmail.com>
2021-11-06 00:05:29 +0700

    two phases included

--- LP_simplex.py
+++ LP_simplex.py
@@ -6,6 +6,9 @@ import numpy as np
 import numpy.ma as ma
 
 def to_tableau(c, A, b):
+    assert A.shape[1] == c.shape[0], 'Length of c and number of columns of A don\'t match'
+    assert A.shape[0] == b.shape[0], 'Length of b and number of rows of A don\'t match'
+
     m, n = A.shape
     tableau = np.zeros((m+1, n+1))
     tableau[:m, :n] = A
@@ -44,24 +47,46 @@ def get_result(tableau, mode):
     return result
 
 def get_basic(tableau):
-    is_basic = np.count_nonzero(tableau[:-1,:-1], axis=0) == 1
+    is_basic = np.count_nonzero(tableau[:,:-1], axis=0) == 1
     return np.where(is_basic)[0]
 
 def cost_row_reduction(tableau):
-    basic_vars = get_basic(tableau)
+    is_basic = np.count_nonzero(tableau[:-1,:-1], axis=0) == 1
+    basic_vars = np.where(is_basic)[0]
     for q in basic_vars:
         p = np.where(tableau[:-1, q] == 1)[0]
         tableau = pivot_step(tableau, (p, q))
     return tableau
 
+def phase_1(c, A, b):
+    A_aux = np.concatenate((A, np.eye(A.shape[0])), axis=1)
+    c_aux = np.concatenate((np.zeros(A.shape[1]), np.ones(A.shape[0])), axis=0)
+    
+    tableau = to_tableau(c_aux, A_aux, b)
+    tableau = cost_row_reduction(tableau)
+
+    print('Phase 1:')
+    tableau = phase_2(tableau)
+
+    return np.delete(tableau, list(range(A.shape[1], A_aux.shape[1])), axis=1)[:-1]
+    
 def LP_simplex(c, A, b, mode):
     assert mode in ['maximize', 'minimize'], 'maximize or minimize?'
     if mode == 'maximize':
         c = -c
-    
+
     tableau = to_tableau(c, A, b)
+    if get_basic(tableau).shape[0] < A.shape[0]:
+        tableau[:-1] = phase_1(c, A, b)
+
     tableau = cost_row_reduction(tableau)
 
+    print('Phase 2:')
+    tableau = phase_2(tableau)
+
+    return get_result(tableau, mode)
+
+def phase_2(tableau):
     iter = 0
     while can_be_improved(tableau):
         iter += 1
@@ -69,17 +94,16 @@ def LP_simplex(c, A, b, mode):
         pivot_position = get_pivot_position(tableau)
         print(f'Iter: {iter}, Pivot: {pivot_position}')
         tableau = pivot_step(tableau, pivot_position)
-
     print(tableau)
-    return get_result(tableau, mode)
+    return tableau
 
 if __name__ == "__main__":
-    c = np.array([3, -1, 3, 0, 0, 0])
-    A = np.array([[2, 1, 1, 1, 0, 0],
-                  [1, 2, 3, 0, 1, 0],
-                  [2, 2, 1, 0, 0, 1],])
-    b = np.array([2, 5, 6])
-    result = LP_simplex(c, A, b, 'minimize')
+    # c = np.array([3, -1, 3, 0, 0, 0])
+    # A = np.array([[2, 1, 1, 1, 0, 0],
+    #               [1, 2, 3, 0, 1, 0],
+    #               [2, 2, 1, 0, 0, 1],])
+    # b = np.array([2, 5, 6])
+    # result = LP_simplex(c, A, b, 'minimize')
 
     # c = np.array([3, 2, 0, 0, 0])
     # A = np.array([[2, 1, 1, 0, 0],
@@ -87,5 +111,19 @@ if __name__ == "__main__":
     #               [1,-1, 0, 0, 1]])
     # b = np.array([7, 8, 2])
     # result = LP_simplex(c, A, b, 'maximize')
-    
+
+    # c = np.array([4, 1, 1])
+    # A = np.array([[2, 1, 2],
+    #               [3, 3, 1],])
+    # b = np.array([4, 3])
+    # result = LP_simplex(c, A, b, 'minimize')
+
+    c = np.array([2, 1, 1, 0, 0])
+    A = np.array([[1, 1, 1, 1, 1],
+                  [1, 1, 2, 2, 2],
+                  [1, 1, 0, 0, 0],
+                  [0, 0, 1, 1, 1]])
+    b = np.array([5, 8, 2, 5])
+    result = LP_simplex(c, A, b, 'maximize')
+
     print(f'{result}')
